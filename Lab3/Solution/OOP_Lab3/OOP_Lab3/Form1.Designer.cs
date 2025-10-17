d11fcbac354b633c954ceae064b575f54eca9e21
iWANbpe <vkutsevil2017@gmail.com>
2025-10-17 10:31:58 +0300

    Add solution and exe file in lab3

---
+++ Lab3/Solution/OOP_Lab3/OOP_Lab3/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿
+namespace OOP_Lab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.Dot_StripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.Line_StripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.Rectangle_StripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.Circle_StripButton4 = new System.Windows.Forms.ToolStripButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(25, 25);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Dot_StripButton1,
+            this.Line_StripButton2,
+            this.Rectangle_StripButton3,
+            this.Circle_StripButton4});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1279, 32);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            this.toolStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.toolStrip1_ItemClicked);
+            // 
+            // Dot_StripButton1
+            // 
+            this.Dot_StripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.Dot_StripButton1.Image = global::OOP_Lab3.Properties.Resources.DotIcon;
+            this.Dot_StripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.Dot_StripButton1.Name = "Dot_StripButton1";
+            this.Dot_StripButton1.Size = new System.Drawing.Size(29, 29);
+            this.Dot_StripButton1.Text = "Dot";
+            // 
+            // Line_StripButton2
+            // 
+            this.Line_StripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.Line_StripButton2.Image = global::OOP_Lab3.Properties.Resources.LineIcon;
+            this.Line_StripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.Line_StripButton2.Name = "Line_StripButton2";
+            this.Line_StripButton2.Size = new System.Drawing.Size(29, 29);
+            this.Line_StripButton2.Text = "Line";
+            // 
+            // Rectangle_StripButton3
+            // 
+            this.Rectangle_StripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.Rectangle_StripButton3.Image = global::OOP_Lab3.Properties.Resources.RectIcon;
+            this.Rectangle_StripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.Rectangle_StripButton3.Name = "Rectangle_StripButton3";
+            this.Rectangle_StripButton3.Size = new System.Drawing.Size(29, 29);
+            this.Rectangle_StripButton3.Text = "Rectangle";
+            // 
+            // Circle_StripButton4
+            // 
+            this.Circle_StripButton4.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.Circle_StripButton4.Image = global::OOP_Lab3.Properties.Resources.CircleIcon;
+            this.Circle_StripButton4.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.Circle_StripButton4.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.Circle_StripButton4.Name = "Circle_StripButton4";
+            this.Circle_StripButton4.Size = new System.Drawing.Size(29, 29);
+            this.Circle_StripButton4.Text = "Circle";
+            this.Circle_StripButton4.TextDirection = System.Windows.Forms.ToolStripTextDirection.Horizontal;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 35);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1279, 610);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(1177, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 26);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "113/113";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(1011, 3);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 26);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "                   ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1279, 644);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton Dot_StripButton1;
+        private System.Windows.Forms.ToolStripButton Line_StripButton2;
+        private System.Windows.Forms.ToolStripButton Rectangle_StripButton3;
+        private System.Windows.Forms.ToolStripButton Circle_StripButton4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Lab3/Solution/OOP_Lab3/OOP_Lab3/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace OOP_Lab3
+{
+    public partial class Form1 : Form
+    {
+        private bool isMouseButtonDown = false;
+
+        private ShapePosition shapePosition = new ShapePosition();
+        private Shape currentShape = null;
+        private Shape[] shapes;
+        private int maxShapes = 113;
+
+        private Bitmap bitmap;
+
+        private Dictionary<string, Shape> shapeDictionary = new Dictionary<string, Shape>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            shapes = new Shape[maxShapes];
+            pictureBox1.Image = bitmap;
+            shapePosition.startPostion = Point.Empty;
+
+            shapeDictionary.Add("Dot", new Dot());
+            shapeDictionary.Add("Line", new Line());
+            shapeDictionary.Add("Rectangle", new Rectangle());
+            shapeDictionary.Add("Circle", new Circle());
+
+            UpdateShapesCountText();
+        }
+
+        private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            string shapeName = e.ClickedItem.Name.Split('_')[0];
+            shapeDictionary.TryGetValue(shapeName, out currentShape);
+            UpdateSelectedShapeText(shapeName);
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawImage(bitmap, Point.Empty);
+            if (isMouseButtonDown) currentShape.PreDraw(shapePosition, e.Graphics);
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (GetShapesCount() < maxShapes && currentShape != null)
+            {
+                isMouseButtonDown = true;
+                shapePosition.startPostion = ReadMousePosition();
+            }
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isMouseButtonDown && GetShapesCount() < maxShapes && currentShape != null)
+            {
+                shapePosition.lastPostion = ReadMousePosition();
+                pictureBox1.Invalidate();
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (GetShapesCount() < maxShapes && currentShape != null)
+            {
+                isMouseButtonDown = false;
+                shapePosition.lastPostion = ReadMousePosition();
+                Graphics graphics = Graphics.FromImage(bitmap);
+
+                currentShape.Draw(shapePosition, graphics);
+                AddShape(currentShape);
+                pictureBox1.Invalidate();
+            }
+        }
+
+        private Point ReadMousePosition()
+        {
+            Point screenPos = Cursor.Position;
+            return pictureBox1.PointToClient(screenPos);
+        }
+
+        private void AddShape(Shape shape)
+        {
+            for (int i = 0; i < shapes.Length; i++)
+            {
+                if (shapes[i] == null)
+                {
+                    shapes[i] = shape;
+                    break;
+                }
+            }
+
+            UpdateShapesCountText();
+        }
+
+        private int GetShapesCount()
+        {
+            int shapesCount = 0;
+
+            foreach (Shape shapeInList in shapes)
+            {
+                if (shapeInList != null)
+                {
+                    shapesCount++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return shapesCount;
+        }
+
+        private void UpdateShapesCountText()
+        {
+            label1.Text = GetShapesCount() + "/" + maxShapes;
+        }
+
+        private void UpdateSelectedShapeText(string shapeName)
+        {
+            label2.Text = shapeName;
+        }
+    }
+}
